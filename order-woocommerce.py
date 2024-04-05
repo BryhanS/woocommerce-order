1a4ef8f401de7eb6fd554393255b09f885638c4b
CATAPU <CATAPU@MacBook-Pro-de-usuario.local>
2024-04-05 13:54:24 -0500

    order update

--- order-woocommerce.py
+++ order-woocommerce.py
@@ -21,7 +21,7 @@ wcapi = API(
 )
 
 # %%
-datos = wcapi.get("orders?per_page=50").json()
+datos = wcapi.get("orders?per_page=100").json()
 
 
 # %%
@@ -52,7 +52,36 @@ datos = wcapi.get("orders?per_page=50").json()
 
 
 # %%
+def payment_name(method_pay):
+    if method_pay == 'woo-mercado-pago-basic':
+        return 'Mercado Pago'
+    if method_pay == 'micuentawebstd':
+        return 'Pago Link - Izipay'
+    else:
+        return 'Revisar'
+    
+
+#'_billing_dni'
+def get_meta_data(key,data):
+    for meta_item in data['meta_data']:
+        if meta_item['key'] == key:
+            return meta_item['value']
+    return None
+
+
+def get_payment_id(method_pay,data):
+    if method_pay == 'woo-mercado-pago-basic':
+        return get_meta_data('_Mercado_Pago_Payment_IDs',data)
+    if method_pay == 'micuentawebstd':
+        return get_meta_data('Transaction ID',data)
+    else:
+        return 'Revisar'
+
+# %%
 row = []
+    # order-shipped
+    # if items['status'] != "on-hold":
+
 
 for items in datos:
 
@@ -68,17 +97,19 @@ for items in datos:
     provincia = items['billing']['provincia'] 
     departamento = items['billing']['departamento']
     monto = items['total']
-    phone = items['billing']['phone']
+    phone = items['billing']['phone'].replace(" ","")
     sku = items['line_items'][0]['sku']
     email = items['billing']['email']
-
-
+    payment_method = payment_name(items['payment_method'])
+    dni = get_meta_data('_billing_dni',items)
+    payment_number = str(get_payment_id(items['payment_method'],items))
     
-    row.append([id,status,registro,'-', nombre_completo,'-','-','-','-','-',address,'-',distrito,provincia,departamento,sku,monto,phone[-9:],'-','-','1',email])
+    row.append([id,status,registro,dni, nombre_completo,'-','-','-','-','-',address,'-',distrito,provincia,departamento,sku,monto,phone[-9:],'-','-','1',email,'-',payment_method,'Capturado',payment_number])
 
 # %%
 df = pd.DataFrame(row)
 df = df.sort_values(by=0, ascending=True)
+df
 
 # %%
 if os.path.exists('reporte.csv'):
